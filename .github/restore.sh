60f45f42d574294935f8c0fea8fe262c05a5c6d3
Robert Elliot <rob@lidalia.org.uk>
2020-03-02 23:34:13 +0000

    Put the caching logic back

--- .github/restore.sh
+++ .github/restore.sh
@@ -3,16 +3,21 @@
 set -exuo pipefail
 
 function main() {
-  local tmpdir=$1
-  local cache_tar=$2
+  local cache_tar=$1
+  local use_backup=$2
 
-  ls -lh "$cache_tar"
-  time sudo service docker stop
-  # mv is c. 25 seconds faster than rm -rf here
-  time sudo mv /var/lib/docker "$tmpdir/olddocker"
-  sudo mkdir -p /var/lib/docker
-  time sudo tar -xf "$cache_tar" -C /var/lib/docker
-  time sudo service docker start
+  if [[ -f "$cache_tar" && "$use_backup" == true ]]; then
+    ls -lh "$cache_tar"
+    time sudo service docker stop
+    # mv is c. 25 seconds faster than rm -rf here
+    time sudo mv /var/lib/docker "$(mktemp -d --dry-run)"
+    sudo mkdir -p /var/lib/docker
+    time sudo tar -xf "$cache_tar" -C /var/lib/docker
+    time sudo service docker start
+  else
+    # Slim docker down - comes with 3GB of data we don't want to backup
+    docker system prune -a -f --volumes
+  fi
 }
 
 main "$@"
