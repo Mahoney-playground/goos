3fdd60d1bb63c88ecce5c1d62658481897338144
Robert Elliot <a-relliot@expedia.com>
2020-03-03 10:18:58 +0000

    Made docker caching more like an action

--- .github/actions/docker-cache/restore.sh
+++ .github/actions/docker-cache/restore.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
 set -exuo pipefail
+# shellcheck source=timing.sh
+. "${BASH_SOURCE%/*}/timing.sh"
 
 function main() {
   local cache_tar=$1
-  local use_backup=$2
 
-  if [[ -f "$cache_tar" && "$use_backup" == true ]]; then
+  if [[ -f "$cache_tar" ]]; then
     ls -lh "$cache_tar"
-    time sudo service docker stop
+    timing sudo service docker stop
     # mv is c. 25 seconds faster than rm -rf here
-    time sudo mv /var/lib/docker "$(mktemp -d --dry-run)"
+    timing sudo mv /var/lib/docker "$(mktemp -d --dry-run)"
     sudo mkdir -p /var/lib/docker
-    time sudo tar -xf "$cache_tar" -C /var/lib/docker
-    time sudo service docker start
+    timing sudo tar -xf "$cache_tar" -C /var/lib/docker
+    timing sudo service docker start
   else
     # Slim docker down - comes with 3GB of data we don't want to backup
     docker system prune -a -f --volumes
