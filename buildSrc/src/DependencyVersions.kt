73cd56c3a98551ef799790c0eb1bf415faa1aaad
Robert Elliot <rob@lidalia.org.uk>
2020-03-31 23:21:50 +0100

    Upgrade to kotest 4.0.1

--- buildSrc/src/DependencyVersions.kt
+++ buildSrc/src/DependencyVersions.kt
@@ -2,5 +2,5 @@
 
 const val kotlinVersion = "1.3.70"
 fun kotlinCoroutines(module: String) = "org.jetbrains.kotlinx:kotlinx-coroutines-$module:1.3.3"
-fun kotest(module: String) = "io.kotest:kotest-$module:4.0.0-BETA2"
+fun kotest(module: String) = "io.kotest:kotest-$module:4.0.1"
 fun smack(module: String) = "org.igniterealtime.smack:smack-$module:4.3.4"
