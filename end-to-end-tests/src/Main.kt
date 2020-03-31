8f75c0b2743ae801279244c16e10b9d4301f4961
Robert Elliot <rob@lidalia.org.uk>
2020-03-31 23:22:45 +0100

    Default report dir in end to end tests

--- end-to-end-tests/src/Main.kt
+++ end-to-end-tests/src/Main.kt
@@ -8,26 +8,31 @@ fun main(args: Array<String>) {
   val defaultedArgs = args
     .withDefaultSelectPackage("goos")
     .withDefaultIncludeClassName("^(Test.*|.+[.$]Test.*|.*Tests?|.*Spec)$")
+    .withDefaultReportsDir("build/reports")
 
   ConsoleLauncher.main(*defaultedArgs)
 }
 
 private fun Array<String>.withDefaultSelectPackage(
   pkg: String
-): Array<String> = withDefaultOption("p", "select-package", pkg)
+): Array<String> = withDefaultOption('p', "select-package", pkg)
 
 private fun Array<String>.withDefaultIncludeClassName(
   @Language("RegExp") pattern: String
-): Array<String> = withDefaultOption("n", "include-classname", pattern)
+): Array<String> = withDefaultOption('n', "include-classname", pattern)
+
+private fun Array<String>.withDefaultReportsDir(
+  reportsDir: String
+): Array<String> = withDefaultOption(null, "reports-dir", reportsDir)
 
 private fun Array<String>.withDefaultOption(
-  shortOption: String,
+  shortOption: Char?,
   longOption: String,
   value: String
 ): Array<String> {
 
   val alreadyHasOption =
-    this.contains("-$shortOption") ||
+    (shortOption != null && this.contains("-$shortOption")) ||
       this.any { it.startsWith("--$longOption=") }
 
   return if (alreadyHasOption) this else this + "--$longOption=$value"
