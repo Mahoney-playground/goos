8e3462574ca6aaa4c88dff353b7f8153f30c9017
Robert Elliot <rob@lidalia.org.uk>
2020-04-24 10:03:44 +0100

    Upgrade to kotlin 1.3.72
    Bug fixes

--- buildSrc/src/DependencyVersions.kt
+++ buildSrc/src/DependencyVersions.kt
@@ -1,6 +1,6 @@
 @file:Suppress("unused")
 
-const val kotlinVersion = "1.3.70"
+const val kotlinVersion = "1.3.72"
 fun kotlinCoroutines(module: String) = "org.jetbrains.kotlinx:kotlinx-coroutines-$module:1.3.3"
 fun kotest(module: String) = "io.kotest:kotest-$module:4.0.1"
 fun arrow(module: String) = "io.arrow-kt:arrow-$module:0.10.5"
