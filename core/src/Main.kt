30bce7f0881b7219efc6299ec9465f730382b9ef
Robert Elliot <rob@lidalia.org.uk>
2020-04-28 17:10:27 +0100

    Move non-gui logic out of the GUI
    Makes it more like the GOOS anyway

--- core/src/Main.kt
+++ core/src/Main.kt
@@ -1,5 +1,16 @@
 package goos.core
 
+import org.jivesoftware.smack.ConnectionConfiguration
+import org.jivesoftware.smack.chat.Chat
+import org.jivesoftware.smack.chat.ChatManager
+import org.jivesoftware.smack.packet.Message
+import org.jivesoftware.smack.tcp.XMPPTCPConnection
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration
+import org.jxmpp.jid.EntityBareJid
+import org.jxmpp.jid.impl.JidCreate
+import org.jxmpp.jid.parts.Resourcepart
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
 import java.util.concurrent.CountDownLatch
 import javax.swing.SwingUtilities
 
@@ -11,6 +22,8 @@ class Main(
 ) {
 
   private lateinit var ui: MainWindow
+  private var connection: XMPPTCPConnection? = null
+  private var notToBeGCd: Chat? = null
 
   init {
     startUserInterface()
@@ -18,13 +31,38 @@ class Main(
 
   private fun startUserInterface() {
     SwingUtilities.invokeAndWait {
-      ui = MainWindow(
-        hostname = hostname,
-        username = username,
-        password = password,
-        itemId = itemId
+      ui = MainWindow(this)
+    }
+  }
+
+  internal fun joinAuction() {
+    if (connection == null) {
+      connection = connectTo(
+        hostname,
+        username,
+        password
       )
     }
+
+    disconnectWhenUICloses(connection!!)
+
+    val chat = ChatManager.getInstanceFor(connection)
+      .createChat(auctionId(itemId, hostname)) { _, _ ->
+        SwingUtilities.invokeLater {
+          ui.showStatus(MainWindow.STATUS_LOST)
+        }
+      }
+    notToBeGCd = chat
+
+    chat.sendMessage(Message())
+  }
+
+  private fun disconnectWhenUICloses(connection: XMPPTCPConnection) {
+    ui.addWindowListener(object : WindowAdapter() {
+      override fun windowClosed(e: WindowEvent?) {
+        connection.disconnect()
+      }
+    })
   }
 
   companion object {
@@ -33,6 +71,8 @@ class Main(
     private const val ARG_USERNAME = 1
     private const val ARG_PASSWORD = 2
     private const val ARG_ITEM_ID = 3
+
+    private val AUCTION_RESOURCE = Resourcepart.from("Auction")
 
     @JvmStatic
     fun main(vararg args: String) {
@@ -62,5 +102,28 @@ class Main(
 
       latch.await()
     }
+
+    private fun connectTo(
+      hostname: String,
+      username: String,
+      password: String
+    ): XMPPTCPConnection {
+      val connection = XMPPTCPConnection(
+        XMPPTCPConnectionConfiguration.builder()
+          .setSecurityMode(ConnectionConfiguration.SecurityMode.disabled)
+          .setXmppDomain(
+            JidCreate.domainBareFrom(
+              hostname
+            )
+          )
+          .build()
+      )
+      connection.connect()
+      connection.login(username, password, AUCTION_RESOURCE)
+      return connection
+    }
+
+    private fun auctionId(itemId: String, hostname: String): EntityBareJid =
+      JidCreate.entityBareFrom("auction-$itemId@$hostname/$AUCTION_RESOURCE")
   }
 }
--- core/src/MainWindow.kt
+++ core/src/MainWindow.kt
@@ -1,14 +1,5 @@
 package goos.core
 
-import org.jivesoftware.smack.ConnectionConfiguration.SecurityMode.disabled
-import org.jivesoftware.smack.XMPPConnection
-import org.jivesoftware.smack.chat.ChatManager
-import org.jivesoftware.smack.packet.Message
-import org.jivesoftware.smack.tcp.XMPPTCPConnection
-import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration
-import org.jxmpp.jid.EntityBareJid
-import org.jxmpp.jid.impl.JidCreate
-import org.jxmpp.jid.parts.Resourcepart
 import java.awt.Color.BLACK
 import javax.swing.JButton
 import javax.swing.JFrame
@@ -18,13 +9,8 @@ import javax.swing.SwingUtilities.invokeLater
 import javax.swing.border.LineBorder
 
 class MainWindow(
-  private val hostname: String,
-  private val username: String,
-  private val password: String,
-  private val itemId: String
+  private val main: Main
 ) : JFrame("Auction Sniper") {
-
-  private var connection: XMPPConnection? = null
 
   private val sniperStatus = JLabel(STATUS_INITIAL).apply {
     name = SNIPER_STATUS_NAME
@@ -34,7 +20,7 @@ class MainWindow(
   private val sniperJoinButton = JButton("Join").apply {
     name = SNIPER_JOIN_BUTTON_NAME
     addActionListener {
-      joinAuction()
+      main.joinAuction()
       invokeLater {
         showStatus(STATUS_JOINING)
       }
@@ -55,23 +41,6 @@ class MainWindow(
     isVisible = true
   }
 
-  private fun joinAuction() {
-    if (connection == null) {
-      connection = connectTo(
-        hostname,
-        username,
-        password
-      )
-    }
-    val chat = ChatManager.getInstanceFor(connection)
-      .createChat(auctionId(itemId, hostname)) { _, _ ->
-        invokeLater {
-          showStatus(STATUS_LOST)
-        }
-      }
-    chat.sendMessage(Message())
-  }
-
   fun showStatus(status: String) {
     sniperStatus.text = status
   }
@@ -83,27 +52,5 @@ class MainWindow(
     const val STATUS_LOST: String = "Lost"
     const val STATUS_INITIAL: String = "Ready to join"
     const val STATUS_JOINING: String = "Joining"
-    private val AUCTION_RESOURCE = Resourcepart.from("Auction")
-
-    private fun connectTo(
-      hostname: String,
-      username: String,
-      password: String
-    ): XMPPConnection {
-      val connection = XMPPTCPConnection(
-        XMPPTCPConnectionConfiguration.builder()
-          .setSecurityMode(disabled)
-          .setXmppDomain(JidCreate.domainBareFrom(
-            hostname
-          ))
-          .build()
-      )
-      .connect()
-      connection.login(username, password, AUCTION_RESOURCE)
-      return connection
-    }
-
-    private fun auctionId(itemId: String, hostname: String): EntityBareJid =
-      JidCreate.entityBareFrom("auction-$itemId@$hostname/$AUCTION_RESOURCE")
   }
 }
