62c321a433220d4f91fa3b05e4cb134ee7176ccc
Robert Elliot <rob@lidalia.org.uk>
2020-04-29 15:14:53 +0100

    Extract an AuctionEvent class

--- core/src/AuctionMessageTranslator.kt
+++ core/src/AuctionMessageTranslator.kt
@@ -9,25 +9,31 @@ class AuctionMessageTranslator(
 ) : ChatMessageListener {
   override fun processMessage(chat: Chat?, message: Message) {
 
-    val event = unpackEventFrom(message)
+    val event = AuctionEvent.from(message.body ?: "")
 
-    when (event["Event"]) {
+    when (event.type) {
       "CLOSE" -> { listener.auctionClosed() }
-      "PRICE" -> { handleClose(event) }
+      "PRICE" -> { listener.currentPrice(price = event.price, increment = event.increment) }
     }
   }
+}
 
-  private fun unpackEventFrom(message: Message): Map<String, String> {
-    return (message.body ?: "").toMap(
+private class AuctionEvent(
+  private val fields: Map<String, String>
+) {
+
+  val type = fields.getValue("Event")
+
+  val price get() = getInt("CurrentPrice")
+  val increment get() = getInt("Increment")
+
+  private fun getInt(key: String) = fields.getValue(key).toInt()
+
+  companion object {
+    fun from(message: String): AuctionEvent = AuctionEvent(message.toMap(
       pairDelimiter = ';',
       keyValueSeparator = ':'
-    )
-  }
-
-  private fun handleClose(event: Map<String, String>) {
-    val price = event.getValue("CurrentPrice").toInt()
-    val increment = event.getValue("Increment").toInt()
-    listener.currentPrice(price = price, increment = increment)
+    ))
   }
 }
 
