7781fd37037e666a19d21f4d925bee53bcc7bb47
Robert Elliot <rob@lidalia.org.uk>
2020-04-29 15:52:06 +0100

    Consistent use of return

--- libraries/kotlinlangext/src/strings/StringToMap.kt
+++ libraries/kotlinlangext/src/strings/StringToMap.kt
@@ -3,15 +3,13 @@ package uk.org.lidalia.kotlinlangext.strings
 fun String.toMap(
   pairDelimiter: Char,
   keyValueSeparator: Char
-): Map<String, String> {
-  return split(pairDelimiter)
-    .map { it.split(keyValueSeparator, limit = 2) }
-    .toMap()
-}
+): Map<String, String> =
+  split(pairDelimiter)
+  .map { it.split(keyValueSeparator, limit = 2) }
+  .toMap()
 
-fun List<List<String>>.toMap(): Map<String, String> {
-  return mapNotNull { it.toPair() }.toMap()
-}
+fun List<List<String>>.toMap(): Map<String, String> =
+  mapNotNull { it.toPair() }.toMap()
 
 fun List<String>.toPair(): Pair<String, String>? =
   if (this.size >= 2)
