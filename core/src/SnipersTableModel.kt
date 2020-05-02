f5bc9f1bb6668a1d3405a016d3a57528d0a9aec2
Robert Elliot <rob@lidalia.org.uk>
2020-05-02 16:20:43 +0100

    Make column OO

--- core/src/SnipersTableModel.kt
+++ core/src/SnipersTableModel.kt
@@ -1,9 +1,5 @@
 package goos.core
 
-import goos.core.Column.ITEM_IDENTIFIER
-import goos.core.Column.LAST_BID
-import goos.core.Column.LAST_PRICE
-import goos.core.Column.SNIPER_STATE
 import goos.core.SniperState.BIDDING
 import goos.core.SniperState.JOINING
 import goos.core.SniperState.LOST
@@ -18,12 +14,8 @@ class SnipersTableModel : AbstractTableModel() {
   override fun getColumnCount(): Int = Column.values().size
   override fun getRowCount(): Int = 1
 
-  override fun getValueAt(rowIndex: Int, columnIndex: Int): Any = when (Column.at(columnIndex)) {
-    ITEM_IDENTIFIER -> sniperSnapshot.itemId
-    LAST_PRICE -> sniperSnapshot.lastPrice
-    LAST_BID -> sniperSnapshot.lastBid
-    SNIPER_STATE -> sniperSnapshot.stateText()
-  }
+  override fun getValueAt(rowIndex: Int, columnIndex: Int): Any =
+    Column.at(columnIndex).valueIn(sniperSnapshot)
 
   fun sniperStateChanged(
     newSniperSnapshot: SniperSnapshot
@@ -32,33 +24,29 @@ class SnipersTableModel : AbstractTableModel() {
     fireTableRowsUpdated(0, 0)
   }
 
-  private fun SniperSnapshot.stateText(): String = when (state) {
-    JOINING -> STATE_JOINING
-    BIDDING -> STATE_BIDDING
-    WINNING -> STATE_WINNING
-    LOST -> STATE_LOST
-    WON -> STATE_WON
-  }
-
   companion object {
 
     private val STARTING_UP = SniperSnapshot("", 0, 0, JOINING)
-
-    private const val STATE_LOST: String = "Lost"
-    private const val STATE_BIDDING: String = "Bidding"
-    private const val STATE_JOINING: String = "Joining"
-    private const val STATE_WINNING: String = "Winning"
-    private const val STATE_WON: String = "Won"
   }
 }
 
 enum class Column {
-  ITEM_IDENTIFIER,
-  LAST_PRICE,
-  LAST_BID,
-  SNIPER_STATE;
+  ITEM_IDENTIFIER { override fun valueIn(snapshot: SniperSnapshot) = snapshot.itemId },
+  LAST_PRICE { override fun valueIn(snapshot: SniperSnapshot) = snapshot.lastPrice },
+  LAST_BID { override fun valueIn(snapshot: SniperSnapshot) = snapshot.lastBid },
+  SNIPER_STATE { override fun valueIn(snapshot: SniperSnapshot) = snapshot.stateText() };
+
+  abstract fun valueIn(snapshot: SniperSnapshot): Any
 
   companion object {
     fun at(offset: Int) = values()[offset]
+
+    private fun SniperSnapshot.stateText(): String = when (state) {
+      JOINING -> "Joining"
+      BIDDING -> "Bidding"
+      WINNING -> "Winning"
+      LOST -> "Lost"
+      WON -> "Won"
+    }
   }
 }
