23a49b6a9324716c91223d1d7f810cc426c17b07
Robert Elliot <rob@lidalia.org.uk>
2020-05-04 15:32:17 +0100

    Upgrade kotest & kotlinx-coroutines

--- buildSrc/src/DependencyVersions.kt
+++ buildSrc/src/DependencyVersions.kt
@@ -1,8 +1,8 @@
 @file:Suppress("unused")
 
 const val kotlinVersion = "1.3.72"
-fun kotlinCoroutines(module: String) = "org.jetbrains.kotlinx:kotlinx-coroutines-$module:1.3.3"
-fun kotest(module: String) = "io.kotest:kotest-$module:4.0.4"
+fun kotlinCoroutines(module: String) = "org.jetbrains.kotlinx:kotlinx-coroutines-$module:1.3.5"
+fun kotest(module: String) = "io.kotest:kotest-$module:4.0.5"
 fun arrow(module: String) = "io.arrow-kt:arrow-$module:0.10.5"
 fun smack(module: String) = "org.igniterealtime.smack:smack-$module:4.3.4"
 val mockk = "io.mockk:mockk:1.10.0"
