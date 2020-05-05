c30aa9d60ae1de26fc4189820ceea08a5cbdf627
Robert Elliot <rob@lidalia.org.uk>
2020-05-05 21:21:10 +0100

    Configure idea package prefix

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,11 @@
+ import org.gradle.plugins.ide.idea.model.IdeaModel
+ import org.jetbrains.gradle.ext.ModuleSettings
+ import org.jetbrains.gradle.ext.PackagePrefixContainer
+
 plugins {
   kotlin("jvm")
   id("alt-application-plugin")
+  id("org.jetbrains.gradle.plugin.idea-ext") version "0.7" apply true
 }
 
 dependencies {
@@ -16,4 +21,27 @@ dependencies {
 
 application {
   mainClassName = "goos.core.app.Main"
+}
+
+idea {
+  setPackagePrefix("goos.core")
+}
+
+val IdeaModel.packagePrefix: PackagePrefixContainer
+  get() {
+    val settings: ModuleSettings by (module as ExtensionAware).extensions
+    val packagePrefix: PackagePrefixContainer by (settings as ExtensionAware).extensions
+    return packagePrefix
+  }
+
+fun IdeaModel.setPackagePrefix(prefix: String) {
+
+  val gradleProject = module?.project ?: project?.project
+
+  gradleProject!!.sourceSets
+    .flatMap { it.java.srcDirs }
+    .map { it.relativeTo(gradleProject.projectDir).path }
+    .forEach {
+      packagePrefix[it] = prefix
+    }
 }
