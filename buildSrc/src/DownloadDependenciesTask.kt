772be7d4594925435799d0d9176cf11e666cfdbc
Robert Elliot <rob@lidalia.org.uk>
2020-05-20 08:53:43 +0100

    Trying to cache dowload dependencies

--- buildSrc/src/DownloadDependenciesTask.kt
+++ buildSrc/src/DownloadDependenciesTask.kt
@@ -1,15 +1,25 @@
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 open class DownloadDependenciesTask : DefaultTask() {
 
+  @InputFiles
+  val inputs = project.configurations.names
+    .map { project.configurations.getAt(it) }
+    .filter { it.isCanBeResolved && !it.isDeprecated() }
+
+  @OutputFile
+  val result = project.buildDir.resolve("download_dependencies_result.txt")
+
   @TaskAction
   fun downloadDependencies() {
-    val allDeps = project.configurations.names
-      .map { project.configurations.getAt(it) }
-      .filter { it.isCanBeResolved && !it.isDeprecated() }
+    val allDeps = inputs
       .map { it.resolve().size }
       .sum()
-    println("Resolved all dependencies: $allDeps")
+    val message = "Resolved all dependencies: $allDeps"
+    result.writeText(message)
+    project.logger.lifecycle(message)
   }
 }
