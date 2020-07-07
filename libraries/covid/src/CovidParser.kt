bf9f1cdc229210fbd0eccd5d11f946e892b3e1ee
Robert Elliot <rob@lidalia.org.uk>
2020-07-07 18:35:58 +0100

    Download the data

--- libraries/covid/src/CovidParser.kt
+++ libraries/covid/src/CovidParser.kt
@@ -2,7 +2,9 @@ package goos
 
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
-import java.io.File
+import java.net.URI
+import java.net.URL
+import java.time.LocalDate
 
 @Serializable
 data class CountryDeaths(
@@ -26,7 +28,9 @@ data class AreaCases(
   val dailyLabConfirmedCases: Int? = 0,
   val previouslyReportedDailyCases: Int? = 0,
   val changeInDailyCases: Int? = 0
-)
+) {
+  val dailyCases = dailyLabConfirmedCases ?: 0
+}
 
 @Serializable
 data class Cases(
@@ -34,6 +38,8 @@ data class Cases(
   val regions: List<AreaCases>,
   val ltlas: List<AreaCases>
 )
+
+val rootUri = URI("https://c19downloads.azureedge.net/downloads/json")
 
 @ExperimentalStdlibApi
 fun main() {
@@ -46,7 +52,7 @@ fun main() {
 
 private fun parseDeaths(json: Json) {
   val deaths =
-    json.parse(Deaths.serializer(), File("coronavirus-deaths_latest.json").readText())
+    json.parse(Deaths.serializer(), rootUri.resolve("coronavirus-deaths_latest.json").toURL().readText())
   val totalsByDate = deaths.countries.groupBy { it.reportingDate }.mapValues { (_, countries) ->
     val byName = countries.associateBy { it.areaName }
     (byName["England"]?.dailyChangeInDeaths ?: 0) + (byName["Wales"]?.dailyChangeInDeaths ?: 0)
@@ -57,17 +63,17 @@ private fun parseDeaths(json: Json) {
 }
 
 private fun parseCases(json: Json) {
-  val cases =
-    json.parse(Cases.serializer(), File("coronavirus-cases_latest.json").readText())
-  val totalsByDate = cases.countries.groupBy { it.specimenDate }.mapValues { (_, countries) ->
-    val byName = countries.associateBy { it.areaName }
-    val england = byName["England"] ?: AreaCases("", "", "", 0, 0, 0)
-    val wales = byName["Wales"] ?: AreaCases("", "", "", 0, 0, 0)
-    val previous = (england.previouslyReportedDailyCases ?: 0) + (wales.previouslyReportedDailyCases ?: 0)
-    val change = (england.changeInDailyCases ?: 0) + (wales.changeInDailyCases ?: 0)
-    val confirmedCases = (england.dailyLabConfirmedCases ?: 0) + (wales.dailyLabConfirmedCases ?: 0)
-    "$previous,$change,$confirmedCases"
-  }
+  val cases = loadCases(json)
+  val totalsByDate = cases.countries
+    .groupByDateAndName()
+    .mapValues { (_, byName) ->
+      val england = byName.getCases("England")
+      val wales = byName.getCases("Wales")
+      val previous = (england.previouslyReportedDailyCases ?: 0) + (wales.previouslyReportedDailyCases ?: 0)
+      val change = (england.changeInDailyCases ?: 0) + (wales.changeInDailyCases ?: 0)
+      val confirmedCases = england.dailyCases + wales.dailyCases
+      "$previous,$change,$confirmedCases"
+    }
   totalsByDate.toList().sortedBy { it.first }.forEach { (date, data) ->
     println("$date,$data")
   }
@@ -75,30 +81,45 @@ private fun parseCases(json: Json) {
 
 
 private fun parseLondonCases(json: Json) {
-  val cases =
-    json.parse(Cases.serializer(), File("coronavirus-cases_latest.json").readText())
-  val totalsByDate = cases.regions.groupBy { it.specimenDate }.mapValues { (_, countries) ->
-    val byName = countries.associateBy { it.areaName }
-    val london = byName["London"] ?: AreaCases("", "", "", 0, 0, 0)
-    val confirmedCases = london.dailyLabConfirmedCases ?: 0
-    "$confirmedCases"
-  }
+  val cases = loadCases(json)
+  val totalsByDate = cases.regions
+    .groupByDateAndName()
+    .mapValues { (_, byName) ->
+      val london = byName.getCases("London")
+      london.dailyCases.toString()
+    }
   totalsByDate.toList().sortedBy { it.first }.forEach { (date, data) ->
     println("$date,$data")
   }
 }
 
 private fun parseLocalCases(json: Json) {
-  val cases =
-    json.parse(Cases.serializer(), File("coronavirus-cases_latest.json").readText())
-  val totalsByDate = cases.ltlas.groupBy { it.specimenDate }.mapValues { (_, countries) ->
-    val byName = countries.associateBy { it.areaName }
-    val richmond = byName["Richmond upon Thames"] ?: AreaCases("", "", "", 0, 0, 0)
-    val hounslow = byName["Hounslow"] ?: AreaCases("", "", "", 0, 0, 0)
-    val confirmedCases = (richmond.dailyLabConfirmedCases ?: 0) + (hounslow.dailyLabConfirmedCases ?: 0)
-    "$confirmedCases"
-  }
+  val cases = loadCases(json)
+  val totalsByDate = cases.ltlas
+    .groupByDateAndName()
+    .mapValues { (_, areaCasesNyName) ->
+      val richmond = areaCasesNyName.getCases("Richmond upon Thames")
+      val hounslow = areaCasesNyName.getCases("Hounslow")
+      val kingston = areaCasesNyName.getCases("Kingston upon Thames")
+      val confirmedCases = richmond.dailyCases + hounslow.dailyCases + kingston.dailyCases
+      confirmedCases.toString()
+    }
   totalsByDate.toList().sortedBy { it.first }.forEach { (date, data) ->
     println("$date,$data")
   }
 }
+
+private fun List<AreaCases>.groupByDateAndName(): Map<LocalDate, Map<String, AreaCases>> {
+  return groupBy { LocalDate.parse(it.specimenDate) }
+    .mapValues { (_, areaCases) -> areaCases.associateBy { it.areaName } }
+}
+
+private fun loadCases(json: Json) =
+  json.parse(
+    Cases.serializer(),
+    rootUri.resolve("coronavirus-cases_latest.json").toURL().readText()
+  )
+
+private fun Map<String, AreaCases>.getCases(
+  name: String
+) = this[name] ?: AreaCases("", name, "", 0, 0, 0)
