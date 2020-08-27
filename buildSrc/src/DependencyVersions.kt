440ce8be0b3586820625aa52acde2d8794dac850
Robert Elliot <rob@lidalia.org.uk>
2020-08-27 08:32:48 +0100

    Upgrade kotest to 4.2.2 - tags work again

--- buildSrc/src/DependencyVersions.kt
+++ buildSrc/src/DependencyVersions.kt
@@ -2,7 +2,7 @@
 
 const val kotlinVersion = "1.4.0"
 fun kotlinCoroutines(module: String) = "org.jetbrains.kotlinx:kotlinx-coroutines-$module:1.3.9"
-fun kotest(module: String) = "io.kotest:kotest-$module:4.2.0"
+fun kotest(module: String) = "io.kotest:kotest-$module:4.2.2"
 fun arrow(module: String) = "io.arrow-kt:arrow-$module:0.10.5"
 fun smack(module: String) = "org.igniterealtime.smack:smack-$module:4.3.4"
 val mockk = "io.mockk:mockk:1.10.0"
