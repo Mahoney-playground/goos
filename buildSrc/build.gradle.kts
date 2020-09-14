c5b4dc60bacbd6574a19b84116338051dc07def8
Robert Elliot <rob@lidalia.org.uk>
2020-09-14 15:39:42 +0100

    Upgrade to kotlin 1.4.10

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  kotlin("jvm") version "1.4.0"
+  kotlin("jvm") version "1.4.10"
   `kotlin-dsl`
   id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
   `java-gradle-plugin`
--- buildSrc/src/DependencyVersions.kt
+++ buildSrc/src/DependencyVersions.kt
@@ -1,6 +1,6 @@
 @file:Suppress("unused")
 
-const val kotlinVersion = "1.4.0"
+const val kotlinVersion = "1.4.10"
 fun kotlinCoroutines(module: String) = "org.jetbrains.kotlinx:kotlinx-coroutines-$module:1.3.9"
 fun kotest(module: String) = "io.kotest:kotest-$module:4.2.2"
 fun arrow(module: String) = "io.arrow-kt:arrow-$module:0.10.5"
