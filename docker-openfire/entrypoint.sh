20eff11d2932c6edd0912c78d7fb06a60cf32c24
Robert Elliot <rob@lidalia.org.uk>
2020-09-15 11:49:24 +0100

    Reformat

--- docker-openfire/entrypoint.sh
+++ docker-openfire/entrypoint.sh
@@ -3,31 +3,31 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-function append_config {
+append_config() {
   local configuration_file=$1
   local config=$2
   sed -i "\|</jive>|i \ \ \ \ $config" "$configuration_file"
 }
 
-function setup_openfire {
+setup_openfire() {
   local configuration_file=$1
-  if ! grep '<setup>true</setup>' "$configuration_file" ; then
+  if ! grep '<setup>true</setup>' "$configuration_file"; then
     append_config "$configuration_file" '<connectionProvider><className>org.jivesoftware.database.EmbeddedConnectionProvider</className></connectionProvider>'
     append_config "$configuration_file" '<setup>true</setup>'
   fi
 }
 
-function run_openfire {
-    exec java \
-      -server \
-      -Dlog4j.configurationFile=/usr/share/openfire/lib/log4j2.xml \
-      -DopenfireHome=/usr/share/openfire \
-      -Dopenfire.lib.dir=/usr/share/openfire/lib \
-      -classpath /usr/share/openfire/lib/startup.jar \
-      -jar /usr/share/openfire/lib/startup.jar
+run_openfire() {
+  exec java \
+    -server \
+    -Dlog4j.configurationFile=/usr/share/openfire/lib/log4j2.xml \
+    -DopenfireHome=/usr/share/openfire \
+    -Dopenfire.lib.dir=/usr/share/openfire/lib \
+    -classpath /usr/share/openfire/lib/startup.jar \
+    -jar /usr/share/openfire/lib/startup.jar
 }
 
-function main {
+main() {
   setup_openfire /usr/share/openfire/conf/openfire.xml
 
   # default behaviour is to launch openfire
