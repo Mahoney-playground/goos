48d52060819bc69c40430346cd30b3edf492b404
Robert Elliot <rob@lidalia.org.uk>
2020-10-07 21:38:45 +0100

    Add doctype & lang to index files

--- buildSrc/src/uk/org/lidalia/indexhtml/IndexFiles.kt
+++ buildSrc/src/uk/org/lidalia/indexhtml/IndexFiles.kt
@@ -10,17 +10,16 @@ import kotlinx.html.div
 import kotlinx.html.h1
 import kotlinx.html.head
 import kotlinx.html.html
+import kotlinx.html.lang
 import kotlinx.html.meta
-import kotlinx.html.stream.createHTML
+import kotlinx.html.stream.appendHTML
 import kotlinx.html.style
 import kotlinx.html.title
 import kotlinx.html.unsafe
 import java.io.File
 
-fun File.addIndexFiles() {
-  runBlocking(Dispatchers.Default) {
-    this@addIndexFiles.addIndexFiles(false)
-  }
+fun File.addIndexFiles() = runBlocking(Dispatchers.Default) {
+  this@addIndexFiles.addIndexFiles(false)
 }
 
 private suspend fun File.addIndexFiles(includeParentLink: Boolean) {
@@ -42,44 +41,47 @@ private fun File.addIndexFile(includeParentLink: Boolean) =
 
 private fun File.indexFileHtml(
   includeParentLink: Boolean
-): String = createHTML()
-  .html {
-    head {
-      title(name)
-      meta {
-        charset = "utf-8"
-      }
-      style {
-        unsafe {
-          +"""
+): String = buildString {
+  appendln("<!DOCTYPE html>")
+  appendHTML()
+    .html {
+      lang = "en"
+      head {
+        title(name)
+        meta {
+          charset = "utf-8"
+        }
+        style {
+          unsafe {
+            +"""
           a {
             text-decoration: none;
           }
           """
-        }
-      }
-    }
-    body {
-      h1 { +"Index of $name" }
-      div {
-        if (includeParentLink) {
-          div {
-            a(href = "..") { +"⬆ Parent Directory" }
-          }
-        }
-        val children = listFiles().orEmpty()
-          .filterNot { it.isHidden }
-          .directoriesFirst()
-        children.forEach { child ->
-          val icon = if (child.isDirectory) "\uD83D\uDCC1" else "\uD83D\uDCC4"
-          div {
-            a(href = child.name) { +"$icon ${child.name}" }
           }
         }
       }
+      body {
+        h1 { +"Index of $name" }
+        div {
+          if (includeParentLink) {
+            div {
+              a(href = "..") { +"⬆ Parent Directory" }
+            }
+          }
+          val children = listFiles().orEmpty()
+            .filterNot { it.isHidden }
+            .directoriesFirst()
+          children.forEach { child ->
+            val icon = if (child.isDirectory) "\uD83D\uDCC1" else "\uD83D\uDCC4"
+            div {
+              a(href = child.name) { +"$icon ${child.name}" }
+            }
+          }
+        }
+      }
     }
-  }
-  .toString()
+}
 
 private fun List<File>.directoriesFirst(): List<File> {
   val (directories, files) = partition { it.isDirectory }
--- libraries/indexhtml/src/IndexFiles.kt
+++ libraries/indexhtml/src/IndexFiles.kt
@@ -10,8 +10,9 @@ import kotlinx.html.div
 import kotlinx.html.h1
 import kotlinx.html.head
 import kotlinx.html.html
+import kotlinx.html.lang
 import kotlinx.html.meta
-import kotlinx.html.stream.createHTML
+import kotlinx.html.stream.appendHTML
 import kotlinx.html.style
 import kotlinx.html.title
 import kotlinx.html.unsafe
@@ -40,44 +41,47 @@ private fun File.addIndexFile(includeParentLink: Boolean) =
 
 private fun File.indexFileHtml(
   includeParentLink: Boolean
-): String = createHTML()
-  .html {
-    head {
-      title(name)
-      meta {
-        charset = "utf-8"
-      }
-      style {
-        unsafe {
-          +"""
+): String = buildString {
+  appendLine("<!DOCTYPE html>")
+  appendHTML()
+    .html {
+      lang = "en"
+      head {
+        title(name)
+        meta {
+          charset = "utf-8"
+        }
+        style {
+          unsafe {
+            +"""
           a {
             text-decoration: none;
           }
           """
-        }
-      }
-    }
-    body {
-      h1 { +"Index of $name" }
-      div {
-        if (includeParentLink) {
-          div {
-            a(href = "..") { +"⬆ Parent Directory" }
-          }
-        }
-        val children = listFiles().orEmpty()
-          .filterNot { it.isHidden }
-          .directoriesFirst()
-        children.forEach { child ->
-          val icon = if (child.isDirectory) "\uD83D\uDCC1" else "\uD83D\uDCC4"
-          div {
-            a(href = child.name) { +"$icon ${child.name}" }
           }
         }
       }
+      body {
+        h1 { +"Index of $name" }
+        div {
+          if (includeParentLink) {
+            div {
+              a(href = "..") { +"⬆ Parent Directory" }
+            }
+          }
+          val children = listFiles().orEmpty()
+            .filterNot { it.isHidden }
+            .directoriesFirst()
+          children.forEach { child ->
+            val icon = if (child.isDirectory) "\uD83D\uDCC1" else "\uD83D\uDCC4"
+            div {
+              a(href = child.name) { +"$icon ${child.name}" }
+            }
+          }
+        }
+      }
     }
-  }
-  .toString()
+}
 
 private fun List<File>.directoriesFirst(): List<File> {
   val (directories, files) = partition { it.isDirectory }
