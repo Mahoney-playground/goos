78ad813bfc6a3964b565ae02da5f96511206897c
Robert Elliot <rob@lidalia.org.uk>
2020-12-15 23:23:00 +0000

    Fix concurrent modification error

--- libraries/kotlinlangext/src/notifiers/Notifier.kt
+++ libraries/kotlinlangext/src/notifiers/Notifier.kt
@@ -1,8 +1,10 @@
 package uk.org.lidalia.kotlinlangext.notifiers
 
+import java.util.concurrent.CopyOnWriteArrayList
+
 open class Notifier<T> {
 
-  private val listeners = mutableListOf<T>()
+  private val listeners = CopyOnWriteArrayList<T>()
   fun addListener(listener: T) = listeners.add(listener)
   fun clear() = listeners.clear()
 
