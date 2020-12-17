6216b7c5770bd401aa98f605ac8a3c935288057a
Robert Elliot <rob@lidalia.org.uk>
2020-12-17 20:59:59 +0000

    Upgrade docker-java-api

--- buildSrc/src/DependencyVersions.kt
+++ buildSrc/src/DependencyVersions.kt
@@ -1,7 +1,7 @@
 @file:Suppress("unused")
 
 fun kotlinCoroutines(module: String) = "org.jetbrains.kotlinx:kotlinx-coroutines-$module:1.3.9"
-fun kotest(module: String) = "io.kotest:kotest-$module:4.2.2"
+fun kotest(module: String) = "io.kotest:kotest-$module:4.3.2"
 fun arrow(module: String) = "io.arrow-kt:arrow-$module:0.11.0"
 fun smack(module: String) = "org.igniterealtime.smack:smack-$module:4.3.4"
 fun jxmpp(module: String) = "org.jxmpp:jxmpp-$module:0.6.4"
--- libraries/testcontainers/build.gradle.kts
+++ libraries/testcontainers/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
   api("org.testcontainers:testcontainers:1.15.0-rc2")
-  implementation("com.github.docker-java:docker-java-api:3.2.5")
+  implementation("com.github.docker-java:docker-java-api:3.2.7")
 }
 
 idea {
