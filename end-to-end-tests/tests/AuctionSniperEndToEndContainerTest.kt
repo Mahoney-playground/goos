534d5b3bff5d38116abbecc71b1041108298ae08
Robert Elliot <rob@lidalia.org.uk>
2021-02-27 15:04:28 +0000

    Allow running AuctionSniperEndToEndTest for real
    Against containers

--- end-to-end-tests/tests/AuctionSniperEndToEndContainerTest.kt
+++ end-to-end-tests/tests/AuctionSniperEndToEndContainerTest.kt
@@ -14,7 +14,7 @@ import java.nio.file.Paths
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
-internal class AuctionSniperEndToEndTestContainerTest : StringSpec({
+internal class AuctionSniperEndToEndContainerTest : StringSpec({
 
   tags(Docker)
 
