64f75500b2432635279e0954163e4449dd754e22
Robert Elliot <rob@lidalia.org.uk>
2021-03-13 13:39:38 +0000

    Improve IdeaExtensions.kt style

--- buildSrc/src/IdeaExtensions.kt
+++ buildSrc/src/IdeaExtensions.kt
@@ -7,26 +7,26 @@ import org.gradle.plugins.ide.idea.model.IdeaModel
 import org.jetbrains.gradle.ext.ModuleSettings
 import org.jetbrains.gradle.ext.PackagePrefixContainer
 
-val IdeaModel.packagePrefix: PackagePrefixContainer
-  get() {
-    val settings: ModuleSettings by (module as ExtensionAware).extensions
-    val packagePrefix: PackagePrefixContainer by (settings as ExtensionAware).extensions
-    return packagePrefix
-  }
-
-val Project.sourceSets: SourceSetContainer
-  get() =
-    (this as ExtensionAware).extensions.getByName("sourceSets") as SourceSetContainer
-
 fun IdeaModel.setPackagePrefix(prefix: String) {
 
   val gradleProject = module?.project ?: project?.project
 
   val srcDirs = gradleProject!!.sourceSets
-    .flatMap { it.java.srcDirs }
+    .flatMap { it.allSource.srcDirs }
     .map { it.relativeTo(gradleProject.projectDir).path }
 
   srcDirs.forEach {
     packagePrefix[it] = prefix
   }
 }
+
+private val IdeaModel.packagePrefix: PackagePrefixContainer
+  get() {
+    val settings: ModuleSettings by (module as ExtensionAware).extensions
+    val packagePrefix: PackagePrefixContainer by (settings as ExtensionAware).extensions
+    return packagePrefix
+  }
+
+private val Project.sourceSets: SourceSetContainer
+  get() =
+    (this as ExtensionAware).extensions.getByName("sourceSets") as SourceSetContainer
