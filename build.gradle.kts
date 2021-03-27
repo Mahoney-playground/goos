f1c145bbfca15313442cfeacc794a73fecf2af03
Robert Elliot <rob@lidalia.org.uk>
2021-03-27 21:52:24 +0000

    Upgrade dep analysis plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ plugins {
   id("uk.org.lidalia.idea-ext") apply false
   id("uk.org.lidalia.download-dependencies")
   id("uk.org.lidalia.copy-without-version")
-  id("com.autonomousapps.dependency-analysis") version "0.70.0"
+  id("com.autonomousapps.dependency-analysis") version "0.71.0"
   id("org.jmailen.kotlinter") version "3.4.0"
   id("com.vanniktech.dependency.graph.generator") version "0.5.0"
   id("com.github.ben-manes.versions") version "0.36.0"
