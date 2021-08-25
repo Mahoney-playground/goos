f66c39693ea50a2f2d16cba925b972da848f026d
Robert Elliot <rob@lidalia.org.uk>
2021-08-25 17:03:46 +0100

    Bring versions into line

--- gradle/build-plugins/idea-ext/build.gradle.kts
+++ gradle/build-plugins/idea-ext/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   `kotlin-dsl`
   `java-gradle-plugin`
-  id("org.jmailen.kotlinter") version "3.4.0"
+  id("org.jmailen.kotlinter") version "3.5.0"
 }
 
 repositories {
--- libraries/indexhtml/build.gradle.kts
+++ libraries/indexhtml/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  kotlin("jvm") version "1.4.31"
-  id("org.jmailen.kotlinter") version "3.4.0"
+  kotlin("jvm") version "1.5.30"
+  id("org.jmailen.kotlinter") version "3.5.0"
   id("uk.org.lidalia.idea-ext")
   id("uk.org.lidalia.kotlin-flat")
 }
@@ -13,7 +13,7 @@ group = "uk.org.lidalia"
 version = "0.1.0"
 
 dependencies {
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
   implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.3")
 }
 
