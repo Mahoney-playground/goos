c137fb715443b37b85d5477e4cb5d13212574182
Robert Elliot <rob@lidalia.org.uk>
2021-09-27 09:28:03 +0100

    Clarify DownloadDependenciesPlugin

--- gradle/build-plugins/download-dependencies/src/main/kotlin/uk/org/lidalia/gradle/plugins/downloaddeps/DownloadDependenciesPlugin.kt
+++ gradle/build-plugins/download-dependencies/src/main/kotlin/uk/org/lidalia/gradle/plugins/downloaddeps/DownloadDependenciesPlugin.kt
@@ -2,21 +2,29 @@ package uk.org.lidalia.gradle.plugins.downloaddeps
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskReference
 import org.gradle.kotlin.dsl.register
 
 class DownloadDependenciesPlugin : Plugin<Project> {
 
   override fun apply(project: Project) {
-    val task = project.tasks.register<DownloadDependenciesTask>("downloadDependencies")
+
+    val task = project.tasks.register<DownloadDependenciesTask>(name)
+
     if (project.isRoot) {
-      project.gradle.includedBuilds.forEach { includedBuild ->
-        task.configure {
-          val includedBuildTask = includedBuild.task(":downloadDependencies")
-          dependsOn(includedBuildTask)
-        }
+      task.configure {
+        dependsOn(*project.includedBuildTasks(":$name"))
       }
     }
+  }
+
+  companion object {
+    const val name = "downloadDependencies"
   }
 }
 
 private val Project.isRoot get() = parent == null
+
+private fun Project.includedBuildTasks(name: String): Array<TaskReference> = gradle.includedBuilds
+  .map { it.task(name) }
+  .toTypedArray()
