92362b4e19d6fbfed0472bd502c1eeffabd36f0b
Robert Elliot <rob@lidalia.org.uk>
2021-12-22 14:00:29 +0000

    Add notes on Either.flatMap

--- gradle/build-plugins/extract-plugin/src/either/Either.kt
+++ gradle/build-plugins/extract-plugin/src/either/Either.kt
@@ -4,6 +4,8 @@ sealed class Either<out L, out R> {
   abstract fun <R2> map(f: (R) -> R2): Either<L, R2>
   abstract fun <L2> mapLeft(f: (L) -> L2): Either<L2, R>
 
+//  abstract fun <L2, R2> flatMap(f: (R) -> Either<L2, R2>): Either<L2, R2>
+
   abstract val isLeft: Boolean
   val isRight: Boolean get() = !isLeft
   abstract fun orNull(): R?
@@ -13,6 +15,10 @@ data class Left<out L>(val value: L) : Either<L, Nothing>() {
   override val isLeft = true
   override fun <R2> map(f: (Nothing) -> R2): Either<L, R2> = this
   override fun <L2> mapLeft(f: (L) -> L2): Either<L2, Nothing> = Left(f(value))
+
+  // cannot compile, see https://youtrack.jetbrains.com/issue/KT-209
+//  override fun <L2, R2> flatMap(f: (Nothing) -> Either<L2, R2>): Either<L, Nothing> where L : L2 = this
+
   override fun orNull() = null
 }
 
@@ -20,6 +26,9 @@ data class Right<R>(val value: R) : Either<Nothing, R>() {
   override val isLeft = false
   override fun <R2> map(f: (R) -> R2): Either<Nothing, R2> = Right(f(value))
   override fun <L2> mapLeft(f: (Nothing) -> L2): Either<L2, R> = this
+
+//  override fun <L2, R2> flatMap(f: (R) -> Either<L2, R2>): Either<L2, R2> = f(value)
+
   override fun orNull() = value
 }
 
@@ -41,7 +50,13 @@ fun <A> Either<A, A>.join(): A = when (this) {
   is Right<A> -> value
 }
 
-inline fun <L, R, R2> Either<L, R>.flatMap(f: (R) -> Either<L, R2>): Either<L, R2> =
+/**
+ * This CANNOT be turned into an abstract member function of `Either` because `L` is covariant, and
+ * we cannot (yet) declare that `L : L2`. See https://youtrack.jetbrains.com/issue/KT-209 .
+ */
+inline fun <L, L2, R, R2> Either<L, R>.flatMap(
+  f: (R) -> Either<L2, R2>
+): Either<L2, R2> where L : L2 =
   when (this) {
     is Right -> f(value)
     is Left -> this
