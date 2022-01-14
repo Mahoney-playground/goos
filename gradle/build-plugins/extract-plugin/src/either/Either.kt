e768632d09eb1db75b51e2dff76aaae42a4258a2
Robert Elliot <rob@lidalia.org.uk>
2022-01-14 15:49:53 +0000

    Improved Either

--- gradle/build-plugins/extract-plugin/src/either/Either.kt
+++ gradle/build-plugins/extract-plugin/src/either/Either.kt
@@ -1,63 +1,74 @@
 package uk.org.lidalia.gradle.plugins.extractplugin.either
 
 sealed class Either<out L, out R> {
-  abstract fun <R2> map(f: (R) -> R2): Either<L, R2>
-  abstract fun <L2> mapLeft(f: (L) -> L2): Either<L2, R>
-
-//  abstract fun <L2, R2> flatMap(f: (R) -> Either<L2, R2>): Either<L2, R2>
 
   abstract val isLeft: Boolean
   val isRight: Boolean get() = !isLeft
-  abstract fun orNull(): R?
+
+  abstract fun <L2> mapLeft(f: (L) -> L2): Either<L2, R>
+  abstract fun <R2> mapRight(f: (R) -> R2): Either<L, R2>
+
+//  abstract fun <L2, R2> flatMapLeft(f: (L) -> Either<L2, R2>): Either<L2, R2>
+//  abstract fun <L2, R2> flatMapRight(f: (R) -> Either<L2, R2>): Either<L2, R2>
+
+  abstract fun leftOrNull(): L?
+  abstract fun rightOrNull(): R?
 }
 
-data class Left<out L>(val value: L) : Either<L, Nothing>() {
+data class Left<L>(val value: L) : Either<L, Nothing>() {
+
   override val isLeft = true
-  override fun <R2> map(f: (Nothing) -> R2): Either<L, R2> = this
-  override fun <L2> mapLeft(f: (L) -> L2): Either<L2, Nothing> = Left(f(value))
 
+  override fun <L2> mapLeft(f: (L) -> L2): Left<L2> = Left(f(value))
+  override fun <R2> mapRight(f: (Nothing) -> R2): Left<L> = this
+
+//  override fun <L2, R2> flatMapLeft(f: (L) -> Either<L2, R2>): Either<L2, R2> = f(value)
   // cannot compile, see https://youtrack.jetbrains.com/issue/KT-209
-//  override fun <L2, R2> flatMap(f: (Nothing) -> Either<L2, R2>): Either<L, Nothing> where L : L2 = this
+//  override fun <L2, R2> flatMapRight(f: (Nothing) -> Either<L2, R2>): Left<L> where L : L2 = this
 
-  override fun orNull() = null
+  override fun leftOrNull() = value
+  override fun rightOrNull() = null
 }
+
+fun <L> L.left() = Left(this)
 
 data class Right<R>(val value: R) : Either<Nothing, R>() {
+
   override val isLeft = false
-  override fun <R2> map(f: (R) -> R2): Either<Nothing, R2> = Right(f(value))
-  override fun <L2> mapLeft(f: (Nothing) -> L2): Either<L2, R> = this
 
-//  override fun <L2, R2> flatMap(f: (R) -> Either<L2, R2>): Either<L2, R2> = f(value)
+  override fun <L2> mapLeft(f: (Nothing) -> L2): Right<R> = this
+  override fun <R2> mapRight(f: (R) -> R2): Right<R2> = Right(f(value))
 
-  override fun orNull() = value
+  // cannot compile, see https://youtrack.jetbrains.com/issue/KT-209
+//  override fun <L2, R2> flatMapLeft(f: (Nothing) -> Either<L2, R2>): Right<R> where R : R2 = this
+//  override fun <L2, R2> flatMapRight(f: (R) -> Either<L2, R2>): Either<L2, R2> = f(value)
+
+  override fun leftOrNull() = null
+  override fun rightOrNull() = value
 }
 
-typealias Outcome<F, S> = Either<F, S>
-typealias Failure<F> = Left<F>
-typealias Success<S> = Right<S>
-val <F, S> Outcome<F, S>.isFailure get() = isLeft
-val <F, S> Outcome<F, S>.isSuccess get() = isRight
-fun <F> F.failure() = Failure(this)
-fun <S> S.success() = Success(this)
-fun <F : Throwable, S> Outcome<F, S>.orThrow(): S = when (this) {
-  is Failure -> throw value
-  is Success -> value
-}
-fun <F, S> Outcome<F, S>.orThrow(f: (F) -> Throwable): S = mapLeft(f).orThrow()
+fun <R> R.right() = Right(this)
 
-fun <A> Either<A, A>.join(): A = when (this) {
-  is Left<A> -> value
-  is Right<A> -> value
+inline fun <L, L2, R, R2> Either<L, R>.flatMapLeft(
+  f: (L) -> Either<L2, R2>
+): Either<L2, R2> where R : R2 = when (this) {
+  is Right -> this
+  is Left -> f(value)
 }
 
 /**
  * This CANNOT be turned into an abstract member function of `Either` because `L` is covariant, and
  * we cannot (yet) declare that `L : L2`. See https://youtrack.jetbrains.com/issue/KT-209 .
  */
-inline fun <L, L2, R, R2> Either<L, R>.flatMap(
+inline fun <L, L2, R, R2> Either<L, R>.flatMapRight(
   f: (R) -> Either<L2, R2>
-): Either<L2, R2> where L : L2 =
-  when (this) {
-    is Right -> f(value)
-    is Left -> this
-  }
+): Either<L2, R2> where L : L2 = when (this) {
+  is Right -> f(value)
+  is Left -> this
+}
+
+@Suppress("unused")
+fun <A> Either<A, A>.join(): A = when (this) {
+  is Left<A> -> value
+  is Right<A> -> value
+}
---
+++ gradle/build-plugins/extract-plugin/src/either/Outcome.kt
@@ -0,0 +1,29 @@
+@file:Suppress("NOTHING_TO_INLINE", "unused")
+
+package uk.org.lidalia.gradle.plugins.extractplugin.either
+
+typealias Outcome<F, S> = Either<F, S>
+
+typealias Success<S> = Right<S>
+inline fun <S> S.success(): Success<S> = right()
+
+typealias Failure<F> = Left<F>
+inline fun <F> F.failure(): Failure<F> = left()
+
+inline val <F, S> Outcome<F, S>.isSuccess: Boolean get() = isRight
+inline val <F, S> Outcome<F, S>.isFailure: Boolean get() = isLeft
+
+inline fun <F, S, S2> Outcome<F, S>.map(noinline f: (S) -> S2): Outcome<F, S2> = mapRight(f)
+inline fun <F, F2, S> Outcome<F, S>.mapFailure(noinline f: (F) -> F2): Outcome<F2, S> = mapLeft(f)
+
+inline fun <F, F2, S, S2> Outcome<F, S>.flatMap(f: (S) -> Either<F2, S2>): Outcome<F2, S2> where F : F2 = flatMapRight(f)
+inline fun <F, F2, S, S2> Outcome<F, S>.flatMapFailure(f: (F) -> Either<F2, S2>): Outcome<F2, S2> where S : S2 = flatMapLeft(f)
+
+inline fun <F, S> Outcome<F, S>.orNull(): S? = rightOrNull()
+
+fun <F : Throwable, S> Outcome<F, S>.orThrow(): S = when (this) {
+  is Failure -> throw value
+  is Success -> value
+}
+
+fun <F, S> Outcome<F, S>.orThrow(f: (F) -> Throwable): S = mapFailure(f).orThrow()
