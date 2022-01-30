b70d72cc69940dbc1212b02aaf24e5ac0d5209cc
Robert Elliot <rob@lidalia.org.uk>
2022-01-30 15:25:26 +0000

    Allow Kotlin DefinitelyNonNullableTypes
    Means you can take a type parameter that could be null and return the
    associated definitely non-null type.
    
    e.g.
    ```kotlin
    fun <T> doSomething(input: T): T & Any
    
    val x: String?
    val y: String = doSomething(x)
    ```

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ import com.github.gundy.semver4j.model.Version
 import com.vanniktech.dependency.graph.generator.DependencyGraphGeneratorExtension.Generator
 import org.gradle.api.distribution.plugins.DistributionPlugin.TASK_INSTALL_NAME
 import org.gradle.internal.deprecation.DeprecatableConfiguration
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jmailen.gradle.kotlinter.KotlinterPlugin
 import uk.org.lidalia.gradle.plugins.copywithoutversion.CopyWithoutVersionsTask
 import uk.org.lidalia.gradle.plugins.downloaddeps.DownloadDependenciesPlugin
@@ -81,6 +82,10 @@ subprojects {
 
       withType<Jar>().configureEach {
         archiveVersion.convention(null as String?)
+      }
+
+      withType<KotlinCompile> {
+        kotlinOptions.freeCompilerArgs = listOf("-XXLanguage:+DefinitelyNonNullableTypes")
       }
 
       named<Test>("test") {
